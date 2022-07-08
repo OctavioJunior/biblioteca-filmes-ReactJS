cf76d4056160ddb1fe29febd102c4d9afe84e29c
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-07-08 00:10:26 -0300

    Adicionando filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import MovieCard from "../components/MovieCard"
 
 const movieURL = import.meta.env.VITE_API
 const apiKEY = import.meta.env.VITE_API_KEY
@@ -10,10 +11,29 @@ const Home = () => {
         const res = await fetch(url)
         const data = await res.json()
 
-        console.log(data)
+        setTopMovies(data.results)
     }
 
-    return <div>Home</div>
+    useEffect(() => {
+        const topRatedURL = `${movieURL}top_rated?${apiKEY}`
+
+        getTopRatedMovies(topRatedURL)
+
+    }, [])
+
+
+
+    return (
+    <div className="container">
+        <h2 className="title">
+            Melhores Filmes:
+        </h2>
+        <div className="moviesContainer">
+            {topMovies.length === 0 && <p>Carregando...</p>}
+            {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id}  movie={movie} />)}
+        </div>
+    </div>
+    )
 }
 
 export default Home
